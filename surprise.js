a619f318a9948d9a3466ee3dfe204624ab7a7b10
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-09-29 21:08:48 +0200

    Polished the JS and made some styling (display)

--- surprise.js
+++ surprise.js
@@ -1,13 +1,13 @@
 const excuse = document.querySelector('.excuse')
 const excuseButton = document.getElementsByClassName('btn')
 
-const characters = ['Linus Torvalds', 'un homme en costume de Pikachu', 'le prof de data', 'Kad Merad', 'un témoin de Jéhovah', 'Jacques Chirac', 'un chat qui parle']
-const verbs = ['a volé', 'a dansé avec', 'a testé un tour de magie avec', 'a défenestré', "m'a échangé ses cartes Pokémon contre", 'revendait des faux pass sanitaires contre']
-const objects = ['mon dernier ticket de métro', 'mes chaussures', 'mes chaussettes', 'mon ordinateur portable', 'une tronçonneuse', 'mon appareil à raclette', 'un paquet de clopes', 'une chemise à carreaux']
+const characters = ['Linus Torvalds', 'un homme en costume de Pikachu', 'le prof de data', 'Kad Merad', 'un témoin de Jéhovah', 'Jacques Chirac', 'un chat noir', "une bactérie de l'évier", 'Thomas', 'Paul-Emmanuel']
+const verbs = ['a volé', 'a dansé avec', 'a testé un tour de magie avec', 'a défenestré', "m'a échangé ses cartes Pokémon contre", "voulait s'enfuir de la Wild avec", 'revendait des faux pass sanitaires contre', "m'a fait passer la douane en cachant", "m'a proposé de tourner un film sur", 'me poursuivait avec']
+const objects = ['mon dernier ticket de métro', 'mes chaussures', 'mes chaussettes', 'mon ordinateur portable', 'une tronçonneuse', 'mon appareil à raclette', 'un paquet de clopes', 'une chemise à carreaux', 'toutes les machines à café']
 
 excuseButton[0].addEventListener('click', function(){
     excuse.innerHTML = ''
-    let excuseElement = document.createElement("li");
+    let excuseElement = document.createElement("p");
 
     let charNum = Math.floor(Math.random()*characters.length)
     let verNum = Math.floor(Math.random()*verbs.length)
@@ -16,6 +16,4 @@ excuseButton[0].addEventListener('click', function(){
 
     excuseElement.innerHTML = sentence;
     excuse.appendChild(excuseElement);
-
-
 })
