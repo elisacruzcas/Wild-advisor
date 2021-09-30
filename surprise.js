870e7010b326fa553eab9ebd0c529a4a107f36e9
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-09-30 15:04:18 +0200

    Created the search resultats part (only displayed onclick of the search button)

--- surprise.js
+++ surprise.js
@@ -1,10 +1,21 @@
+// SEARCH ENGINE PART
+const results = document.querySelector('.search-results')
+const resultsButton = document.querySelector('.first-button')
+
+resultsButton.addEventListener('click', function(){
+    results.style.display = 'block'
+})
+
+
+// BAD EXCUSES PART
+
 const excuse = document.querySelector('.excuse')
 const excuseBox = document.querySelector('.excuses-bidons')
 const excuseButton = document.getElementsByClassName('btn')
 
-const characters = ['Linus Torvalds', 'un homme en costume de Pikachu', 'le prof de data', 'Kad Merad', 'un témoin de Jéhovah', 'Jacques Chirac', 'un chat noir', "une bactérie de l'évier", 'Thomas', 'Paul-Emmanuel']
-const verbs = ['a volé', 'a dansé avec', 'a testé un tour de magie avec', 'a défenestré', "m'a échangé ses cartes Pokémon contre", "voulait s'enfuir de la Wild avec", 'revendait des faux pass sanitaires contre', "m'a fait passer la douane en cachant", "m'a proposé de tourner un film sur", 'me poursuivait avec']
-const objects = ['mon dernier ticket de métro', 'mes chaussures', 'mes chaussettes', 'mon ordinateur portable', 'une tronçonneuse', 'mon appareil à raclette', 'un paquet de clopes', 'une chemise à carreaux', 'toutes les machines à café']
+const characters = ['Tu Duy', 'Linus Torvalds', 'un homme en costume de Pikachu', 'le prof de data', 'Kad Merad', 'un témoin de Jéhovah', 'Jacques Chirac', 'un chat noir', "une bactérie de l'évier", 'Thomas', 'Paul-Emmanuel']
+const verbs = ['a volé', 'a créé des actions en Bourse sur', 'a dansé avec', 'a testé un tour de magie avec', 'a défenestré', "m'a échangé ses cartes Pokémon contre", "voulait s'enfuir de la Wild avec", 'revendait des faux pass sanitaires contre', "m'a fait passer la douane en cachant", "m'a proposé de tourner un film sur", 'me poursuivait avec']
+const objects = ['mon dernier ticket de métro', 'mes chaussures', 'mes chaussettes', 'mon ordinateur portable', 'une tronçonneuse', 'mon appareil à raclette', 'un paquet de clopes', 'une chemise à carreaux', 'toutes les machines à café', 'les viennoiseries sur la table']
 
 excuseButton[0].addEventListener('click', function(){
     excuse.innerHTML = ''
