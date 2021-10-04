e63e1406c314cb9dbe4f0a21794a6580a7429cd4
elisacruzcas <elisa.castrillo.cruz@gmail.com>
2021-10-04 22:05:09 +0200

    Improving menu

--- web/menu.js
+++ web/menu.js
@@ -1,10 +1,10 @@
-const menuList = document.getElementsByClassName('menu');
-const menuBtn = document.getElementsByClassName('icon-menu');
+//const menuList = document.getElementsByClassName('menu');
+//const menuBtn = document.getElementsByClassName('icon-menu');
 
-menuBtn[0].addEventListener('click', function(){
-  if (menuList[0].style.display == "none"){
-    menuList[0].style.display = "block"}
-  else {
-    menuList[0].style.display = "none"
-  }
-})
+//menuBtn[0].addEventListener('click', function(){
+ // if (menuList[0].style.display == "none"){
+  //  menuList[0].style.display = "block"}
+ // else {
+  //  menuList[0].style.display = "none"
+ // }
+//})
