40dc60648c64ba77d3c139266d8df8e8efa837a0
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-04 12:08:53 +0200

    Now the button can close the navbar too

--- web/menu.js
+++ web/menu.js
@@ -1,8 +1,10 @@
 const menuList = document.getElementsByClassName('menu');
 const menuBtn = document.getElementsByClassName('icon-menu');
 
-
-
 menuBtn[0].addEventListener('click', function(){
-  menuList[0].style.display = "block"
+  if (menuList[0].style.display == "none"){
+    menuList[0].style.display = "block"}
+  else {
+    menuList[0].style.display = "none"
+  }
 })
