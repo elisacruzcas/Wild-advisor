7cf9da72da2222570dc81eb903c6c59950648e19
VincentCMLejeune <vincentcmlejeune@gmail.com>
2021-10-06 15:10:27 +0200

    Fixed the JS header issue, created a specific js file to store excuses elements, styled the box

---
+++ pages-header.js
@@ -0,0 +1,9 @@
+// JS SPECIFIQUE AU HEADER
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
---
+++ surprise-data.js
@@ -0,0 +1,48 @@
+function getCharacters(){
+    return ['Tu Duy',
+'Linus Torvalds',
+'un homme en costume de Pikachu', 
+'le prof de data', 
+"Muad'Dib", 
+'un témoin de Jéhovah', 
+'Jacques Chirac', 
+'un chat noir', 
+"une bactérie de l'évier", 
+'Thomas', 
+'Paul-Emmanuel',
+'Brendan Eich',
+'Elon Musk',
+'Edouard Philippe'
+]}
+
+function getVerbs() {
+    return ['a volé', 
+'a créé des actions en Bourse sur', 
+'a dansé avec', 
+'a testé un tour de magie avec', 
+'a défenestré', 
+"m'a échangé ses cartes Pokémon contre", 
+"voulait s'enfuir de la Wild avec", 
+'revendait des faux pass sanitaires contre', 
+"m'a fait passer la douane en cachant", 
+"m'a proposé de tourner un film sur", 
+'me poursuivait avec',
+'dessinait',
+]}
+
+function getObjects() {
+    return ['mon dernier ticket de métro', 
+'mes chaussures', 
+'mes chaussettes', 
+'mon ordinateur portable', 
+'une tronçonneuse', 
+'mon appareil à raclette', 
+'un paquet de clopes', 
+'une chemise à carreaux', 
+'toutes les machines à café', 
+'les viennoiseries sur la table',
+'la banane pourrie au fond du frigo',
+'la dernière boite de post-its'
+]}
+
+export {getCharacters, getVerbs, getObjects}
