271f8883d01e63d28cf4a6a5b22fa51db5facc3a
gonzalezjc <jcobasgonzalez@gmail.com>
2021-10-07 20:28:19 +0200

    responsive 100% font, colors etc added footer and header integrated

--- propos.js
+++ propos.js
@@ -1,20 +1,32 @@
-const textO = document.getElementsByClassName('teamuno');
-const profileP = document.getElementsByClassName('photouno');
 
-const textOdos = document.getElementsByClassName('teamdos');
-const profilePdos = document.getElementsByClassName('photodos');
+// JS SPECIFIQUE AU HEADER
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
 
-const textOtres = document.getElementsByClassName('teamtres');
-const profilePtres = document.getElementsByClassName('phototres');
 
-const textOcuatro = document.getElementsByClassName('teamcuatro');
-const profilePcuatro = document.getElementsByClassName('photocuatro');
+// const textO = document.getElementsByClassName('teamuno');
+// const profileP = document.getElementsByClassName('photouno');
 
-const textOcinco = document.getElementsByClassName('teamcinco');
-const profilePcinco = document.getElementsByClassName('photocinco');
+// const textOdos = document.getElementsByClassName('teamdos');
+// const profilePdos = document.getElementsByClassName('photodos');
 
-const textOseis = document.getElementsByClassName('teamseis');
-const profilePseis = document.getElementsByClassName('photoseis');
+// const textOtres = document.getElementsByClassName('teamtres');
+// const profilePtres = document.getElementsByClassName('phototres');
+
+// const textOcuatro = document.getElementsByClassName('teamcuatro');
+// const profilePcuatro = document.getElementsByClassName('photocuatro');
+
+// const textOcinco = document.getElementsByClassName('teamcinco');
+// const profilePcinco = document.getElementsByClassName('photocinco');
+
+// const textOseis = document.getElementsByClassName('teamseis');
+// const profilePseis = document.getElementsByClassName('photoseis');
 
 profileP[0].addEventListener('click', () => {
     if(textO[0].style.display !== "inline") {
@@ -25,21 +37,21 @@ profileP[0].addEventListener('click', () => {
   });
 
 
-profilePdos[0].addEventListener('click', () => {
-    if(textOdos[0].style.display !== "inline") {
-        textOdos[0].style.display = "inline"
-    } else {
-    textOdos[0].style.display = "none"
-    }
-  });
+// profilePdos[0].addEventListener('click', () => {
+//     if(textOdos[0].style.display !== "inline") {
+//         textOdos[0].style.display = "inline"
+//     } else {
+//     textOdos[0].style.display = "none"
+//     }
+//   });
 
-  profilePtres[0].addEventListener('click', () => {
-    if(textOtres[0].style.display !== "inline") {
-        textOtres[0].style.display = "inline"
-    } else {
-    textOtres[0].style.display = "none"
-    }
-  });
+//   profilePtres[0].addEventListener('click', () => {
+//     if(textOtres[0].style.display !== "inline") {
+//         textOtres[0].style.display = "inline"
+//     } else {
+//     textOtres[0].style.display = "none"
+//     }
+//   });
 
   profilePcuatro[0].addEventListener('click', () => {
     if(textOcuatro[0].style.display !== "inline") {
@@ -49,18 +61,18 @@ profilePdos[0].addEventListener('click', () => {
     }
   });
 
-  profilePcinco[0].addEventListener('click', () => {
-    if(textOcinco[0].style.display !== "inline") {
-        textOcinco[0].style.display = "inline"
-    } else {
-    textOcinco[0].style.display = "none"
-    }
-  });
+//   profilePcinco[0].addEventListener('click', () => {
+//     if(textOcinco[0].style.display !== "inline") {
+//         textOcinco[0].style.display = "inline"
+//     } else {
+//     textOcinco[0].style.display = "none"
+//     }
+//   });
 
-  profilePseis[0].addEventListener('click', () => {
-    if(textOseis[0].style.display !== "inline") {
-        textOseis[0].style.display = "inline"
-    } else {
-    textOseis[0].style.display = "none"
-    }
-  });
+//   profilePseis[0].addEventListener('click', () => {
+//     if(textOseis[0].style.display !== "inline") {
+//         textOseis[0].style.display = "inline"
+//     } else {
+//     textOseis[0].style.display = "none"
+//     }
+//   });
